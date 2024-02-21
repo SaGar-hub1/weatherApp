3ef6b819358fef5946eeb3afbf355740e6550a30
Patil Shashikant Bhagwan <111949548+SaGar-hub1@users.noreply.github.com>
2024-02-21 11:06:12 +0530

    Update Myservlet.java

--- src/main/java/mypackage/Myservlet.java
+++ src/main/java/mypackage/Myservlet.java
@@ -44,7 +44,7 @@ public class Myservlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String city = request.getParameter("City");
-		String apiKey = "a9209fcc2a1984a052be33b31796540a";
+		String apiKey = " ADd your open weather API Key";
 		// Create the URL for the OpenWeatherMap API request
 		String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 		// doGet(request, response);
